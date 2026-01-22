5fd1f2a9df7e28423dd2aadf2b6fafaa3e3ff535
HENg DeV <songhengkim93@gmail.com>
2026-01-22 09:56:41 +0000

    Add files via upload

---
+++ ex2 - array duplication.js
@@ -0,0 +1,31 @@
+// 1. We are managing a data structure of students -  representing a student with 'firstName' and 'age' properties.
+// 2. The 'updateStudentAge' function is supposed to update the age of a student his/her first name
+// 3. However, some students have the same first name !
+// 4. To fix this bug, we want to add 2 properties : lastName and batch  (given a firstName + lastName + batch we do not expect duplication)
+
+// TODO:
+// - Update the data strucure and the functions to manage those new properties
+const STUDENTS_DATA = [
+  { firstName: "An", lastname:"chong", batch: 1, age: 20 },
+  { firstName: "Bình",lastname:"norn", batch: 2, age: 22 },
+  { firstName: "Cẩm",lastname:"leng", batch: 4, age: 21 },
+  { firstName: "An",lastname:"oun", batch: 3, age: 19 }, // Duplicate first name !
+];
+
+/**
+ * Update 1 student age, given his/her first name
+ * @param {string} firstName - the student first name
+ * @param {age} newAge  - the student new age
+ */
+function updateStudentAge(firstName, newAge,lastName,Batch) {
+  let student = STUDENTS_DATA.find((s) => s.firstName === firstName && s.lastname === lastName && s.batch === Batch);
+  if (student) {
+    student.age = newAge;
+  }
+}
+
+// 1 - Update An age to 30
+updateStudentAge("An", 30);
+
+// 2 - Print the updated data
+console.log(JSON.stringify(STUDENTS_DATA));
